2183b5d44584d03d3f9df83531e96839efe32b6b
Patrick J. Rex <prexdesigns@gmail.com>
2024-04-21 20:59:41 -0400

    Fixes
    - fixing errors in unused stories

--- src/stories/molecules/Dialog.ts
+++ src/stories/molecules/Dialog.ts
@@ -2,12 +2,11 @@ import { html } from 'lit';
 
 
 export interface DialogProps {
-    mode: boolean;
     open: boolean;
     content: string;
 }
 
-export const Dialog = ({mode, open, content}: DialogProps) => {
+export const Dialog = ({open, content}: DialogProps) => {
     return html `
       <dialog ${open}>
        ${content}
